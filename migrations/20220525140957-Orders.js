86c93ac64d9ddd154113241eed50335a7cb06b84
cesarhro <cesarhr.olimpio@gmail.com>
2022-05-25 11:25:44 -0300

    Create migration Orders

---
+++ migrations/20220525140957-Orders.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id:{
+        type: Sequelize.UUID,
+        primaryKey: true
+      },
+      client_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      service_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      line_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      description:{
+        type: Sequelize.STRING(150),
+        allowNull: false
+      },
+      amount_itens:{
+        type: Sequelize.INTEGER, 
+        allowNull: false
+      },
+      procedure_id:{
+        type: Sequelize.UUID,
+        allowNull: false
+      },  
+      payment_id:{
+        type: Sequelize.UUIDV4,
+        allowNull: false
+      },
+      status:{
+        type: Sequelize.ENUM('concluded','in progress'),
+        allowNull: false
+      },
+      created_at:{
+        type: Sequelize.DATE, 
+        allowNull: false
+      },
+      updated_at:{
+        type: Sequelize.DATE, 
+        allowNull: false
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
