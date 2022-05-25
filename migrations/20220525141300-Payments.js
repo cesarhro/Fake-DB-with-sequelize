665fec223b97fcd2f639faf4060a6e6096275c4f
cesarhro <cesarhr.olimpio@gmail.com>
2022-05-25 11:26:12 -0300

    Create migration Payments

---
+++ migrations/20220525141300-Payments.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id:{
+        type: Sequelize.UUID,
+        primaryKey: true
+      },
+      order_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      value:{
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+      client_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      payment_type_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      status:{
+        type: Sequelize.ENUM('in process','accepted','denied'), 
+        allowNull: false
+      },
+      created_at:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at:{
+        type: Sequelize.DATE,
+        allowNull: false
+      } 
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  }
+};
