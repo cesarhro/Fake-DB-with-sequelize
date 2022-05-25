c1a8fe2e63552f7d0378d2d9b21f100071d20037
KDU15 <carloskc127@gmail.com>
2022-05-25 10:18:11 -0300

    Created model Document_types

---
+++ models/model_Document_Types.js
@@ -0,0 +1,26 @@
+'use strict'
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+    const Document_Types = sequelize.define('Document_Types', {
+        id:{
+            field: 'id',
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        document:{
+            field: 'document',
+            type: Sequelize.STRING(30),
+            allowNull: false
+        }    
+    },
+    {
+        tablename: 'Document_Types'
+    })
+    
+    /* Line_Type.associate = function (models) {
+        //fazer os imports aqui
+    } */
+
+    return Document_Types
+}
