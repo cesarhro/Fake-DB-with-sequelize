692ae08dd7f9eafa7c10a303bc60a0ff3aac3ebb
cesarhro <cesarhr.olimpio@gmail.com>
2022-05-25 10:36:21 -0300

    Create model Clients

---
+++ models/model_Clients.js
@@ -0,0 +1,57 @@
+'use strict'
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+    const Clients = sequelize.define('Clients', {
+        id:{
+            field: 'id',
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        full_name:{
+            field: 'full_name',
+            type: Sequelize.STRING(200),
+            allowNull: false
+        },
+        email:{
+            field: 'value',
+            type: Sequelize.STRING(100),
+            allowNull: false
+        },
+        doc_type_id:{
+            field: 'doc_type_id',
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        doc_number:{
+            field: 'doc_number',
+            type: Sequelize.STRING(30),
+            allowNull: false
+        },
+        address_id:{
+            field: 'address_id',
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        birth_date:{
+            field: 'birth_date',
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        phone:{
+            field: 'phone',
+            type: Sequelize.BIGINT,
+            allowNull: false
+        },
+    },
+    {
+        timestamps: false,
+        tablename: 'Clients'
+    })
+    
+    /* Clients.associate = function (models) {
+        //fazer os imports aqui
+    } */
+
+    return Clients
+}
